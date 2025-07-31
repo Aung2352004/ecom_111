c897987d82298141b72b21669c3e35a823b4edd7
Aung2352004 <siaung128@gmail.com>
2025-07-31 11:58:35 +0630

    function tested

--- admin/index.php
+++ admin/index.php
@@ -5,4 +5,14 @@ $name = "IMC";//string data type
 $temp = 33.5;//float data type
 echo "<b><em>Hello Batch $batch, welcome to ecommerce module</em></b>";
 
+function power($num1, $num2)
+{
+    return pow($num1,$num2);
+}
+
+$number1 = 2;
+$number2 = 6;
+$result = power($number1, $number2);//calling function
+echo "<br>result is $result"
+
 ?>
